da50cc41f6a6f6ec0de889a0b69e0defcce16fdb
sashamelnyk0 <sashamel098@gmail.com>
2021-10-26 10:01:22 +0300

    Create nastupnabukva.py

---
+++ nastupnabukva.py
@@ -0,0 +1,12 @@
+alfavit_zifri = 'abcdefghijklmnopqrstuvwxyz'
+zbilshenya = int(input('На скільки збільшити: '))
+message = input("Повідомлення: ")
+widpovid = ''
+for a in message:
+    old_bukva = alfavit_zifri.find(a)
+    new_bukva = old_bukva + zbilshenya
+    if a in alfavit_zifri:
+        widpovid += alfavit_zifri[new_bukva]
+    else:
+        widpovid += a
+print (widpovid)
